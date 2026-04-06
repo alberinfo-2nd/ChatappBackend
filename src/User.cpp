c08e1f64a468dd2992c439da3a16c307b8ecc6a8
mills0911 <mills0911@gmail.com>
2026-04-05 20:18:36 -0400

    Bug fix for std::string return

--- src/User.cpp
+++ src/User.cpp
@@ -34,7 +34,7 @@ void User::report(void) {
 AdminUser::AdminUser(std::string username, std::string password, std::string public_key) : User(username, public_key) {
     std::fstream admin_users("../admins.cfg");
     if(!admin_users.is_open()) {
-        throw "There was an error opening the when looking up the administrators!";
+        throw std::string("There was an error opening the when looking up the administrators!");
     }
 
     //Check file for user
