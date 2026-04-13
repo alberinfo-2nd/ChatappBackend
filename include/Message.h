c5276ed1b924fd2682705da8954df6ce3aa06216
James <mills0911@gmail.com>
2026-04-13 15:05:17 -0400

    Added timestamp for messages

--- include/Message.h
+++ include/Message.h
@@ -1,6 +1,6 @@
 #include <string>
-//#include <chrono> // For handling time
-//#include <ctime>  // For converting time to readable strings    
+#include <chrono> // For handling time
+#include <ctime>  // For converting time to readable strings    
 #include <json.hpp>
 
 class Message {
@@ -13,12 +13,8 @@ public:
     //Constructors
     //Message() : timestamp(std::chrono::system_clock::now()) {}
     Message(std::string recipient, std::string body) 
-        : recipientUsername(recipient), content(body) /*timestamp(std::chrono::system_clock::now()*/{}
+        : recipientUsername(recipient), content(body), timestamp(std::chrono::system_clock::now()){}
     
-    /* Returns the raw timestamp
-    std::chrono::system_clock::time_point getTimestamp() const {
-        return timestamp;
-    }*/
     // Get Functions
     const std::string& getSender() const{
         return(recipientUsername);
@@ -30,11 +26,13 @@ public:
     // Return String of formatted timestap
     std::string getTimeStamp() const{
         std::time_t time = std::chrono::system_clock::to_time_t(timestamp);
-        return std::ctime(&time);
+        std::string s = std::ctime(&time);
+        if (!s.empty()) s.pop_back();  //removes randomm "\n"
+        return s;  
     }
 
     const nlohmann::json toJSON() const {
-        return {{"sender", getSender()}, {"content", getContent()}};
+        return {{"timestamp", getTimeStamp()}, {"sender", getSender()}, {"content", getContent()}};
     }
 
 };
